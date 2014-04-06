4d72e64c392203b15e30819ba96e92b465f23bcd
Chris Block <chris.block@gmail.com>
2014-04-05 21:15:03 -0500

    Tweaked the test macro and added more implementations of it.

--- source/orm.cpp.integration_tests/DoubleColumnEntityTests.cpp
+++ source/orm.cpp.integration_tests/DoubleColumnEntityTests.cpp
@@ -1,38 +1,14 @@
 #include "stdafx.h"
 
-#include <initializer_list>
-
-#include <environment.h>
-#include <connection.h>
-#include <statement.h>
-
-#include <ClassMap.h>
-#include <ExpressionOperators.h>
-#include <MakeExpression.h>
-#include <MappingRegistrar.h>
-#include <Session.h>
-
-#include "TestColumnEntity.h"
-#include "TestColumnEntityMap.h"
-#include "TestMappingRegistrar.h"
 #include "TestColumnTestFixture.h"
 
-#define CONNECTION_STRING	"Driver={SQL Server Native Client 11.0}; Server=(local); Database=OdbcCppTestDatabase; Trusted_Connection=Yes;"
-
-#define TEST_FOR_COLUMN_TYPE(ColumnType, SqlType, DefaultValue, TestValue1, TestValue2, TestValue3) \
-	DECLARE_TEST_COLUMN_ENTITY(ColumnType) \
-	IMPLEMENT_TEST_COLUMN_ENTITY(ColumnType, DefaultValue) \
-	DECLARE_TEST_COLUMN_ENTITY_MAP(ColumnType) \
-	IMPLEMENT_TEST_COLUMN_ENTITY_MAP(ColumnType) \
-	DECLARE_TEST_MAPPING_REGISTRAR(ColumnType) \
-	IMPLEMENT_TEST_MAPPING_REGISTRAR(ColumnType) \
-	IMPLEMENT_TEST_FIXTURE(ColumnType, SqlType) \
-	IMPLEMENT_SELECT_TEST(ColumnType, TestValue1, TestValue2, TestValue3) \
-	IMPLEMENT_INSERT_TEST(ColumnType, TestValue1) \
-	IMPLEMENT_UPDATE_TEST(ColumnType, TestValue1, TestValue2, TestValue3) \
-	IMPEMENT_DELETE_TEST(ColumnType, TestValue1, TestValue2, TestValue3)
+using namespace std;
 
 TEST_FOR_COLUMN_TYPE(bool, "BIT", false, true, false, true)
+
+TEST_FOR_COLUMN_TYPE(int8_t, "TINYINT", 0, 1, 2, 3)
+
+TEST_FOR_COLUMN_TYPE(int16_t, "SMALLINT", 0, 1, 2, 3)
 
 TEST_FOR_COLUMN_TYPE(int32_t, "INT", 0, 1, 2, 3)
 
@@ -41,3 +17,9 @@ TEST_FOR_COLUMN_TYPE(int64_t, "BIGINT", 0LL, 1LL, 2LL, 3LL)
 TEST_FOR_COLUMN_TYPE(float, "REAL", 0.0f, 1.2f, 3.4f, 4.5f)
 
 TEST_FOR_COLUMN_TYPE(double, "FLOAT", 0.0, 1.2, 3.4, 4.5)
+
+TEST_FOR_COLUMN_TYPE(string, "VARCHAR(255)", "", "Hello, World.", "Goodbye, World.", "Look! A Test!")
+
+TEST_FOR_COLUMN_TYPE(wstring, "NVARCHAR(255)", L"", L"Hello, World.", L"Goodbye, World.", L"Look! A Test!")
+
+TEST_FOR_COLUMN_TYPE(Guid, "UNIQUEIDENTIFIER", Guid::Empty, Guid::NewGuid(), Guid::NewGuid(), Guid::NewGuid())
--- source/orm.cpp.integration_tests/TestColumnTestFixture.h
+++ source/orm.cpp.integration_tests/TestColumnTestFixture.h
@@ -1,7 +1,24 @@
 #pragma once
 
+#include <initializer_list>
 #include <string>
 #include <vector>
+
+#include <connection.h>
+#include <environment.h>
+#include <parameter.h>
+#include <statement.h>
+
+#include <ClassMap.h>
+#include <ExpressionOperators.h>
+#include <Guid.h>
+#include <MakeExpression.h>
+#include <MappingRegistrar.h>
+#include <Session.h>
+
+#include "TestColumnEntity.h"
+#include "TestColumnEntityMap.h"
+#include "TestMappingRegistrar.h"
 
 #define IMPLEMENT_TEST_FIXTURE(ColumnType, ColumnSqlType) \
 	class ColumnType ## _ColumnTests : public TestColumnTestFixture \
@@ -9,7 +26,7 @@
 	protected: \
 		virtual std::string GetConnectionString() const \
 		{ \
-			std::string connectionString(CONNECTION_STRING); \
+			std::string connectionString("Driver={SQL Server Native Client 11.0}; Server=(local); Database=OdbcCppTestDatabase; Trusted_Connection=Yes;"); \
 	\
 			return connectionString; \
 		}; \
@@ -40,7 +57,11 @@
 #define IMPLEMENT_SELECT_TEST(ColumnType, TestValue1, TestValue2, TestValue3) \
 	TEST_F(ColumnType ## _ColumnTests, Select) \
 	{ \
-		std::vector<ColumnType> values = { TestValue1, TestValue2, TestValue3 }; \
+		const ColumnType variable1 = TestValue1; \
+		const ColumnType variable2 = TestValue2; \
+		const ColumnType variable3 = TestValue3; \
+	\
+		std::vector<ColumnType> values = { variable1, variable2, variable3 }; \
 	\
 		InsertTestRecords<ColumnType>(values); \
 	\
@@ -59,11 +80,11 @@
 		std::vector<ColumnType ## _ColumnTestsEntity>::const_iterator jter = entities.cbegin(); \
 	\
 		EXPECT_EQ(1, entities.at(0).GetId()); \
-		EXPECT_EQ(TestValue1, entities.at(0).GetColumn()); \
+		EXPECT_EQ(variable1, entities.at(0).GetColumn()); \
 		EXPECT_EQ(2, entities.at(1).GetId()); \
-		EXPECT_EQ(TestValue2, entities.at(1).GetColumn()); \
+		EXPECT_EQ(variable2, entities.at(1).GetColumn()); \
 		EXPECT_EQ(3, entities.at(2).GetId()); \
-		EXPECT_EQ(TestValue3, entities.at(2).GetColumn()); \
+		EXPECT_EQ(variable3, entities.at(2).GetColumn()); \
 	}
 
 // TODO: make this work
@@ -86,7 +107,11 @@
 #define IMPLEMENT_UPDATE_TEST(ColumnType, TestValue1, TestValue2, TestValue3) \
 	TEST_F(ColumnType ## _ColumnTests, Update) \
 	{ \
-		std::vector<ColumnType> values = { TestValue1, TestValue2 }; \
+		const ColumnType variable1 = TestValue1; \
+		const ColumnType variable2 = TestValue2; \
+		const ColumnType variable3 = TestValue3; \
+	\
+		std::vector<ColumnType> values = { variable1, variable2 }; \
 	\
 		InsertTestRecords<ColumnType>(values); \
 	\
@@ -95,7 +120,7 @@
 		ColumnType ## _ColumnTestsEntity one; \
 	\
 		one.SetId(2); \
-		one.SetColumn(TestValue3); \
+		one.SetColumn(variable3); \
 	\
 		int32_t numberUpdated = 0; \
 	\
@@ -114,13 +139,17 @@
 		EXPECT_EQ(2, GetRowCount()); \
 	\
 		EXPECT_EQ(2, entities.front().GetId()); \
-		EXPECT_EQ(TestValue3, entities.front().GetColumn()); \
+		EXPECT_EQ(variable3, entities.front().GetColumn()); \
 	}
 
 #define IMPEMENT_DELETE_TEST(ColumnType, TestValue1, TestValue2, TestValue3) \
 	TEST_F(ColumnType ## _ColumnTests, Delete) \
 	{ \
-		std::vector<ColumnType> values = { TestValue1, TestValue2, TestValue3 }; \
+		const ColumnType variable1 = TestValue1; \
+		const ColumnType variable2 = TestValue2; \
+		const ColumnType variable3 = TestValue3; \
+	\
+		std::vector<ColumnType> values = { variable1, variable2, variable3 }; \
 	\
 		InsertTestRecords<ColumnType>(values); \
 	\
@@ -142,6 +171,19 @@
 		EXPECT_EQ(2, numberDeleted); \
 		EXPECT_EQ(1, GetRowCount()); \
 	}
+
+#define TEST_FOR_COLUMN_TYPE(ColumnType, SqlType, DefaultValue, TestValue1, TestValue2, TestValue3) \
+	DECLARE_TEST_COLUMN_ENTITY(ColumnType) \
+	IMPLEMENT_TEST_COLUMN_ENTITY(ColumnType, DefaultValue) \
+	DECLARE_TEST_COLUMN_ENTITY_MAP(ColumnType) \
+	IMPLEMENT_TEST_COLUMN_ENTITY_MAP(ColumnType) \
+	DECLARE_TEST_MAPPING_REGISTRAR(ColumnType) \
+	IMPLEMENT_TEST_MAPPING_REGISTRAR(ColumnType) \
+	IMPLEMENT_TEST_FIXTURE(ColumnType, SqlType) \
+	IMPLEMENT_SELECT_TEST(ColumnType, TestValue1, TestValue2, TestValue3) \
+	IMPLEMENT_INSERT_TEST(ColumnType, TestValue1) \
+	IMPLEMENT_UPDATE_TEST(ColumnType, TestValue1, TestValue2, TestValue3) \
+	IMPEMENT_DELETE_TEST(ColumnType, TestValue1, TestValue2, TestValue3)
 
 class MappingRegistrar;
 class Session;
@@ -169,13 +211,20 @@ protected:
 	{
 		std::string command;
 
+		std::vector<std::shared_ptr<odbc::parameter>> parameters;
+
 		for (std::vector<T>::const_iterator iter = values.cbegin(); iter != values.cend(); iter++)
 		{
+			std::shared_ptr<odbc::parameter> parameter = std::make_shared<odbc::parameter>();
+
+			parameter->set_name("TestColumn");
+			parameter->set(*iter);
+
+			parameters.push_back(parameter);
+
 			command.append("INSERT INTO [dbo].[");
 			command.append(GetTableName());
-			command.append("] ([TestColumn]) VALUES (");
-			command.append(std::to_string(*iter));
-			command.append(")\n");
+			command.append("] ([TestColumn]) VALUES (?)\n");
 		}
 
 		std::shared_ptr<odbc::environment> environment = std::make_shared<odbc::environment>();
@@ -185,6 +234,11 @@ protected:
 		connection->open();
 
 		odbc::statement statement(connection, command);
+
+		for (std::vector<std::shared_ptr<odbc::parameter>>::const_iterator iter = parameters.cbegin(); iter != parameters.cend(); iter++)
+		{
+			statement.add_parameter(*iter);
+		}
 
 		statement.execute();
 
